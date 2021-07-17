260c5a3943a38cb807b104f7ebdb381deba0c436
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-07-18 00:38:11 +0530

    Basic javaScript

---
+++ intro.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+let a;
+a = 10;
+a = "I am String"
+console.log(a);
+for(let i=1;i<=10;i++){
+    console.log("Number is "+ i);
+}
+let b = 5/2;
+console.log(b);
+//single line print
+process.stdout.write("Hello ");
+process.stdout.write("Hello");
+
