b3495bf2a1c8bfac5ffd4ae73cda58939d2ee054
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-07-29 22:31:05 +0530

    Some changes

--- PepDev-1/Lecture-1/intro.js
+++ PepDev-1/Lecture-1/intro.js
@@ -8,7 +8,17 @@ for(let i=1;i<=10;i++){
 }
 let b = 5/2;
 console.log(b);
-//single line print
+//single line print on only condition that it needs string
 process.stdout.write("Hello ");
 process.stdout.write("Hello");
+console.log();
+
+
+//input type cohersion
+//->concatenation converts number into string(+)
+console.log("Hello i am something "+10); //10 is string
+//arithemetic operators converts a string into number(-,<,>,',')
+console.log("Hello i am something",10); //10 is number
+let input = "10";
+console.log(input<5);
 
